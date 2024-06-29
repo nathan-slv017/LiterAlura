821af98bac5d478c07da5ba9a0b21945db77f8fc
Nathan da Silva Barrozo <email.do.natann@gmail.com>
2024-06-29 14:35:45 -0300

    2

--- src/main/java/com/example/leituraApplication/principal/Principal.java
+++ src/main/java/com/example/leituraApplication/principal/Principal.java
@@ -2,13 +2,12 @@ package com.example.leituraApplication.principal;
 
 import com.example.leituraApplication.service.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
 
 @Component
-public class Principal implements CommandLineRunner {
+public class Principal {
 
     private final LivroService livroService;
     private final Scanner scanner = new Scanner(System.in);
@@ -18,18 +17,11 @@ public class Principal implements CommandLineRunner {
         this.livroService = livroService;
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        exibeMenu();
-    }
-
-
     public void exibeMenu() {
         int opcao = -1;
 
-
         while (opcao != 0) {
-            processoOpcao(opcao);
+
             String menu = """
                     Escolha o número da sua opção:
                     1- Buscar livro pelo título
@@ -41,26 +33,25 @@ public class Principal implements CommandLineRunner {
                     """;
             System.out.println(menu);
             opcao = scanner.nextInt();
-            scanner.nextLine(); // Consumir a nova linha pendente
-        }
-    }
+            scanner.nextLine();
+            switch (opcao){
+                case 1:
+                    System.out.println("Digite o titulo do livro: ");
+                    String livroBuscado = scanner.nextLine();
+                    livroService.buscarLivroPorTitulo(livroBuscado);
+                    break;
+                case 2:
+                    livroService.listarLivrosRegistrados();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
 
-    private void processoOpcao(int opcao) {
-        switch (opcao) {
-            case 1:
-                System.out.println("Digite o titulo do livro: ");
-                String livroBuscado = scanner.nextLine();
-                livroService.buscarLivroPorTitulo(livroBuscado);
-                break;
-            case 2:
-                livroService.listarLivrosRegistrados();
-                break;
-            case 0:
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opção inválida. Tente novamente.");
-                break;
+            }
+
         }
     }
 }
--- src/main/java/com/example/leituraApplication/service/ConsumoApi.java
+++ src/main/java/com/example/leituraApplication/service/ConsumoApi.java
@@ -2,25 +2,44 @@ package com.example.leituraApplication.service;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 @Service
 public class ConsumoApi {
 
     private static final String API_URL = "https://gutendex.com/books?search=";
 
-    @Autowired
-    private RestTemplate restTemplate;
-
     public JsonObject buscarLivroPorTitulo(String titulo) {
         try {
-            String query = API_URL + titulo.replace(" ", "+");
-            String response = restTemplate.getForObject(query, String.class);
+            String query = API_URL + URLEncoder.encode(titulo, "UTF-8");
+            URL url = new URL(query);
 
-            Gson gson = new Gson();
-            return gson.fromJson(response, JsonObject.class);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                Gson gson = new Gson();
+                return gson.fromJson(response.toString(), JsonObject.class);
+            } else {
+                System.out.println("GET request not worked");
+                return null;
+            }
         } catch (Exception e) {
             e.printStackTrace();
             return null;
